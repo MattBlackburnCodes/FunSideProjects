0e585935d7324e27b56e4dac15f7ebf6b91546c1
SoliBlack <105514525+SoliBlack@users.noreply.github.com>
2022-05-14 20:41:20 -0400

    Chore Randomizer

---
+++ Chore Randomizer/Chore Randomizer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Chore_Randomizer
+{
+    class Program
+    {
+        static void Chores()
+        {
+            Random r = new Random();
+            // List of chores
+            string[] chores = {"clean the bathrooms", "clean the counters", "vaccuum the floors",
+            "clean the parents bedroom", "clean the kids room", "clean the living room", "clean the laundry room"};
+
+            string randomString = $"{chores[r.Next(0, chores.Length)]}";
+            
+            Console.WriteLine($"Your task is to {randomString}");
+            
+            Selection();
+
+
+
+        }
+
+        static void Selection()
+        {
+            
+            Console.WriteLine("Would you like another task? (y/n): ");
+            //Console.WriteLine("");
+            string choice = "";
+
+            do
+            {
+                choice = Console.ReadLine();
+
+
+                switch (choice)
+                {
+                    case "y":
+                        Console.WriteLine();
+                        Chores();
+                        
+                        break;
+                    case "n":
+                        Console.WriteLine();
+                        Console.WriteLine("Great! Let's get to work!");
+                        //Console.ReadKey(true);
+                        break;
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid input.");
+                        Console.WriteLine("Would you like another task? (y/n): ");
+                        
+                        choice = "x";
+
+                        break;
+
+
+                }
+
+            } while (choice == "x");
+        }
+
+            static void Main(string[] args)
+        {
+            Chores();
+        }
+    }
+}
